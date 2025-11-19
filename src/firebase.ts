6a44d54bc11588cdb4727794836af523458be615
Rodolfo Gama <rodolfo.gama2006@gmail.com>
2025-11-19 20:51:55 -0300

    First Commit

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3TU6BpIGMqLa8JY9abEHrx0DH2L3ngHY",
+  authDomain: "web-mobile-a7.firebaseapp.com",
+  projectId: "web-mobile-a7",
+  storageBucket: "web-mobile-a7.firebasestorage.app",
+  messagingSenderId: "935348669798",
+  appId: "1:935348669798:web:d10f65bd8c9ff7dc6a4ca7"
+};
+
+const app = initializeApp(firebaseConfig);
